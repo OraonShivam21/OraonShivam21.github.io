976ef9efc9f04b2a7bf867406af8d5c78ace0a2a
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-31 02:36:42 +0530

    updating home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,33 @@ import LandscapeStudy from "../assets/landscape-study.png";
 import "../styles/Home.css";
 
 const Home = () => {
+  const resumePath =
+    "https://drive.google.com/file/d/10SF2l87jaZJaF7721jgBaRSclOFfKrVf/view?usp=sharing";
+
   return (
-  <div id="home">
-    <div className="mask">
-      <img className="intro-img" src={LandscapeStudy} alt="landscape-study" />
-    </div>
-    <div className="content">
-      <p>HI, I'M SHIVAM ORAON</p>
-      <h1>NodeJS Developer</h1>
-      <div>
-        <a className="btn nav-link" href="#projects">Projects</a>
-        <a className="btn btn-light" href="#download">Resume</a>
+    <div id="home">
+      <div className="mask">
+        <img className="intro-img" src={LandscapeStudy} alt="landscape-study" />
+      </div>
+      <div className="content">
+        <h1>HI, I'M SHIVAM ORAON</h1>
+        <p>Backend Developer</p>
+        <div className="content-buttons">
+          <a className="btn" href="#projects">
+            Projects
+          </a>
+          <a
+            className="btn btn-light"
+            href={resumePath}
+            target="_blank"
+            rel="noopener noreferrer"
+            download="Shivam_Oraon_Resume.pdf"
+          >
+            Resume
+          </a>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
