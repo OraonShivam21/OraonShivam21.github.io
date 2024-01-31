7bfbdc51b7c6c308397466b89c8afec0e71912bd
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-31 10:59:25 +0530

    building skills section

---
+++ src/assets/technical-skills.js
@@ -0,0 +1,25 @@
+import css3 from "./css-3.png";
+import expressjs from "./expressjs.png";
+import github from "./github.png";
+import html5 from "./html-5.png";
+import javascript from "./javascript.png";
+import mongodb from "./mongodb.png";
+import netlify from "./netlify.png";
+import nodejs from "./nodejs.png";
+import reactjs from "./reactjs.png";
+import vercel from "./vercel.png";
+import vscode from "./vscode.png";
+
+export {
+  css3,
+  expressjs,
+  github,
+  html5,
+  javascript,
+  mongodb,
+  netlify,
+  nodejs,
+  reactjs,
+  vercel,
+  vscode,
+};
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,78 @@
 import React from "react";
+import {
+  css3,
+  expressjs,
+  github,
+  html5,
+  javascript,
+  mongodb,
+  netlify,
+  nodejs,
+  reactjs,
+  vercel,
+  vscode,
+} from "../assets/technical-skills";
+import "../styles/Skills.css";
 
 const Skills = () => {
   return (
-  <div id="skills">
-    <h1 style={{height:"100vh"}}>Skills Section</h1>
-  </div>
+    <div class="skill-stats">
+      <div id="skills">
+        <h1 className="skills-heading">Technical Skills</h1>
+        <div className="technical-skills">
+          <div className="skills-card">
+            <div className="skills-card-img">
+              <img src={html5} alt="html5" />
+            </div>
+            <p className="skills-card-name">HTML 5</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={css3} alt="css3" />
+            <p className="skills-card-name">CSS 3</p>
+          </div>
+          <div className="skills-card">
+            <img
+              className="skills-card-img"
+              src={javascript}
+              alt="javascript"
+            />
+            <p className="skills-card-name">JavaScript</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={nodejs} alt="nodejs" />
+            <p className="skills-card-name">Node.js</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={expressjs} alt="expressjs" />
+            <p className="skills-card-name">Express.js</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={mongodb} alt="mongodb" />
+            <p className="skills-card-name">MongoDB</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={reactjs} alt="reactjs" />
+            <p className="skills-card-name">React</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={github} alt="github" />
+            <p className="skills-card-name">Github</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={vscode} alt="vscode" />
+            <p className="skills-card-name">VS Code</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={vercel} alt="vercel" />
+            <p className="skills-card-name">Vercel</p>
+          </div>
+          <div className="skills-card">
+            <img className="skills-card-img" src={netlify} alt="netlify" />
+            <p className="skills-card-name">Netlify</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
