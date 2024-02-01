b481485fd199a7cd80d3e4eb7db869fcb6079d53
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-01 07:42:24 +0530

    updated projects section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -20,6 +20,46 @@ const Project = () => {
           <div className="project-img-git-link">
             <img
               className="project-image"
+              src={EdifyEducationalApp}
+              alt="edify-educational-app"
+            />
+            <div className="project-git-dep">
+              <a
+                href={EdifyGit}
+                className="project-github-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href={EdifyAppLink}
+                className="project-deployed-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live
+              </a>
+            </div>
+          </div>
+          <div className="project-title-desc-stack">
+            <h2 className="project-title">Edify Educational App</h2>
+            <p className="project-description">
+              Grow you skills to advance your career path - Edify offers you
+              with variety of courses which helps you in your skill development
+              and makes you confident for the job. Enroll and get started. Edify
+              - Education Website is an online platform where Edify offers
+              various courses for users or students who are tech enthusiasts and
+              are willing to either learn a new skill in technical field or are
+              willing to upskill themselves.
+            </p>
+            <h3 className="project-tech-stack">HTML5 | CSS3 | JavaScript</h3>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="project-img-git-link">
+            <img
+              className="project-image"
               src={TrackerJetApp}
               alt="tracker-jet-app"
             />
@@ -45,12 +85,11 @@ const Project = () => {
           <div className="project-title-desc-stack">
             <h2 className="project-title">Tracker Jet App</h2>
             <p className="project-description">
-              The Tracker Jet App is a responsive website that offers students
-              an all-in-one study tracking platform, which helps them with time
-              management while doing their assignments. The students can easily
-              interact with all the functionalities and improve their time
-              management skills while restricting the time for their
-              assignments.
+              The Tracker Jet App offers students an all-in-one study tracking
+              platform, which helps them with time management while doing their
+              assignments. The students can easily interact with all the
+              functionalities and improve their time management skills while
+              restricting the time for their assignments.
             </p>
             <h3 className="project-tech-stack">
               HTML5 | CSS3 | Bootstrap | JavaScript | Node.js | Express.js |
@@ -87,54 +126,13 @@ const Project = () => {
           <div className="project-title-desc-stack">
             <h2 className="project-title">Wonder Wave Travels</h2>
             <p className="project-description">
-              The WonderWave Travels is a responsive website which offers the
-              travellers with the best India Tour packages, where they can tour
-              almost every part of India with the hand-picked packages given in
-              the website.
+              The WonderWave Travels offers the travellers with the best India
+              Tour packages, where they can tour almost every part of India with
+              the hand-picked packages given in the website.
             </p>
             <h3 className="project-tech-stack">
               HTML5 | CSS3 | Bootstrap | JavaScript | JSON-Server
             </h3>
-          </div>
-        </div>
-        <div className="project-card">
-          <div className="project-img-git-link">
-            <img
-              className="project-image"
-              src={EdifyEducationalApp}
-              alt="edify-educational-app"
-            />
-            <div className="project-git-dep">
-              <a
-                href={EdifyGit}
-                className="project-github-link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-              <a
-                href={EdifyAppLink}
-                className="project-deployed-link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live
-              </a>
-            </div>
-          </div>
-          <div className="project-title-desc-stack">
-            <h2 className="project-title">Edify Educational App</h2>
-            <p className="project-description">
-              Grow you skills to advance your career path - Edify offers you
-              with variety of courses which helps you in your skill development
-              and makes you confident for the job. Enroll and get started. Edify
-              - Education Website is an online platform where Edify offers
-              various courses for users or students who are tech enthusiasts and
-              are willing to either learn a new skill in technical field or are
-              willing to upskill themselves.
-            </p>
-            <h3 className="project-tech-stack">HTML5 | CSS3 | JavaScript</h3>
           </div>
         </div>
         {/* <div className="project-card">
